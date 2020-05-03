7409cd1c3073c057b85ae04d21c411e35240e0b5
George Lim <lim.george@pm.me>
2020-05-03 12:03:02 -0400

    Update cookie persistence + add logging

--- examples/send-message/index.js
+++ examples/send-message/index.js
@@ -4,28 +4,32 @@
   const fs = require('fs').promises
 
   let browser = null
+  let page = null
 
   try {
     browser = await puppeteer.launch({ headless: true })
-    const page = await browser.newPage()
+    page = await browser.newPage()
 
+    const client = await page.target().createCDPSession()
     let cookies = null
 
     // Provide existing saved cookies from file
     try {
       const cookiesJSON = await fs.readFile('./cookies.json')
       cookies = JSON.parse(cookiesJSON)
+
+      // Log in and get updated cookies
       await page.setCookie(...cookies)
+      cookies = await bot.login(page, client)
     }
     catch (error) {
-      console.info('Unable to get existing cookies')
-    }
+      console.info('Unable to log in using existing cookies...')
 
-    if (!cookies) {
-      // Provide account credentials for login
+      // Provide account credentials for fallback login
       const username = ''
       const password = ''
-      cookies = await bot.login(page, username, password)
+
+      cookies = await bot.login(page, client, username, password)
     }
 
     // Save cookies to local file
@@ -42,6 +46,11 @@
   }
   catch (error) {
     console.error(error)
+
+    if (page !== null) {
+      await page.screenshot({ path: './error-main.jpg', type: 'jpeg' })
+    }
+
     process.exit(1)
   }
   finally {
--- index.js
+++ index.js
@@ -1,28 +1,50 @@
 module.exports = {
   /**
-   * Log into TextNow
+   * Log into TextNow and renew cookies
    * @param  {object} page     Puppeteer browser page
-   * @param  {string} username Account credential
-   * @param  {string} password Account credential
+   * @param  {object} client   Puppeteer CDPSession
+   * @param  {string} username Optional account credential
+   * @param  {string} password Optional account credential
    * @return {object}          Updated login cookies
    */
-  login: async (page, username, password) => {
+  login: async (page, client, username, password) => {
+    if (username && password) {
+      console.info('Logging in with account credentials...')
+
+      await Promise.all([
+        page.goto('http://textnow.com/login'),
+        page.waitForNavigation({ waitUntil: 'networkidle2' })
+      ])
+
+      await page.type('#txt-username', username)
+      await page.type('#txt-password', password)
+
+      const loginButton = await page.waitForSelector('#btn-login')
+
+      await Promise.all([
+        loginButton.click(),
+        page.waitForNavigation()
+      ])
+
+      const cookies = (await client.send('Network.getAllCookies')).cookies
+      return cookies
+    }
+
+    console.info('Logging in with existing cookies...')
+
     await Promise.all([
-      page.goto('http://textnow.com/login'),
+      page.goto('https://www.textnow.com/login'),
       page.waitForNavigation({ waitUntil: 'networkidle2' })
     ])
 
-    await page.type('#txt-username', username)
-    await page.type('#txt-password', password)
+    const isLoggedIn = page.url().includes('/messaging')
 
-    const loginButton = await page.waitForSelector('#btn-login')
+    if (!isLoggedIn) {
+      throw new Error('Existing cookies are invalid / expired')
+    }
 
-    await Promise.all([
-      loginButton.click(),
-      page.waitForNavigation()
-    ])
-
-    return page.cookies()
+    const cookies = (await client.send('Network.getAllCookies')).cookies
+    return cookies
   },
 
   /**
@@ -31,6 +53,8 @@ module.exports = {
    * @param {string} recipient Recipient contact info
    */
   selectConversation: async(page, recipient) => {
+    console.info('Selecting conversation...')
+
     await Promise.all([
       page.goto('https://www.textnow.com/messaging'),
       page.waitForNavigation({ waitUntil: 'networkidle2' })
@@ -50,6 +74,8 @@ module.exports = {
    * @param {number} delay   Optional delay after sending message (ms)
    */
   sendMessage: async (page, message, delay) => {
+    console.info('Sending message...')
+
     const messageField = await page.waitForSelector('#text-input')
     await messageField.type(message)
     await page.keyboard.press('Enter')
