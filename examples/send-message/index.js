36e9d11effd5971c658b8c929499e958a6f16309
George Lim <lim.george@pm.me>
2020-09-09 16:38:34 -0400

    Clean up codebase

--- examples/send-message/index.js
+++ examples/send-message/index.js
@@ -1,73 +1,73 @@
 (async () => {
-  const puppeteer = require('puppeteer')
-  const textnow = require('textnow-bot')
-  const fs = require('fs').promises
+  const puppeteer = require('puppeteer');
+  const textnow = require('textnow-bot');
+  const fs = require('fs').promises;
 
-  let browser = null
-  let page = null
+  let browser = null;
+  let page = null;
 
   // Account credentials
-  const username = ''
-  const password = ''
+  const username = '';
+  const password = '';
   // Recipient info
-  const recipient = ''
+  const recipient = '';
   // Message info
-  const message = 'hello world'
+  const message = 'message';
 
   try {
-    browser = await puppeteer.launch({ headless: true })
-    page = await browser.newPage()
-    const client = await page.target().createCDPSession()
-    let cookies = null
+    browser = await puppeteer.launch({ headless: true });
+    page = await browser.newPage();
+    const client = await page.target().createCDPSession();
+    let cookies = null;
 
     // Import cookies from file
     try {
-      console.debug('Importing existing cookies...')
-      const cookiesJSON = await fs.readFile('./cookies.json')
-      cookies = JSON.parse(cookiesJSON)
+      console.debug('Importing existing cookies...');
+      const cookiesJSON = await fs.readFile('./cookies.json');
+      cookies = JSON.parse(cookiesJSON);
     }
-    catch (exception) {
-      console.debug('Failed to import existing cookies.')
+    catch (error) {
+      console.debug('Failed to import existing cookies.');
     }
 
     // Log into TextNow and get cookies
     try {
-      console.debug('Logging in with existing cookies...')
-      await page.setCookie(...cookies)
-      cookies = await textnow.logIn(page, client)
+      console.debug('Logging in with existing cookies...');
+      await page.setCookie(...cookies);
+      cookies = await textnow.logIn(page, client);
     }
-    catch (exception) {
-      console.debug('Failed to log in with existing cookies.')
-      console.debug('Logging in with account credentials...')
-      cookies = await textnow.logIn(page, client, username, password)
+    catch (error) {
+      console.debug('Failed to log in with existing cookies.');
+      console.debug('Logging in with account credentials...');
+      cookies = await textnow.logIn(page, client, username, password);
     }
 
     // Save cookies to file
-    console.debug('Successfully logged into TextNow!')
-    await fs.writeFile('./cookies.json', JSON.stringify(cookies))
+    console.debug('Successfully logged into TextNow!');
+    await fs.writeFile('./cookies.json', JSON.stringify(cookies));
 
     // Select a conversation using recipient info
-    console.debug('Selecting conversation...')
-    await textnow.selectConversation(page, recipient)
+    console.debug('Selecting conversation...');
+    await textnow.selectConversation(page, recipient);
 
     // Send a message to the current recipient
-    console.debug('Sending message...')
-    await textnow.sendMessage(page, message)
+    console.debug('Sending message...');
+    await textnow.sendMessage(page, message);
 
-    console.debug('Message sent!')
-    await browser.close()
+    console.debug('Message sent!');
+    await browser.close();
   }
-  catch (exception) {
-    console.error(exception)
+  catch (error) {
+    console.error(error);
 
     if (page) {
-      await page.screenshot({ path: './error-screenshot.jpg', type: 'jpeg' })
+      await page.screenshot({ path: './error-screenshot.jpg', type: 'jpeg' });
     }
 
     if (browser) {
-      await browser.close()
+      await browser.close();
     }
 
-    process.exit(1)
+    process.exit(1);
   }
-})()
+})();
--- index.js
+++ index.js
@@ -10,48 +10,48 @@ module.exports.logIn = async (page, client, username, password) => {
   await Promise.all([
     page.goto('https://www.textnow.com/login'),
     page.waitForNavigation({ waitUntil: 'networkidle2' })
-  ])
+  ]);
 
   if (username && password) {
-    await page.type('#txt-username', username)
-    await page.type('#txt-password', password)
+    await page.type('#txt-username', username);
+    await page.type('#txt-password', password);
 
-    const logInButton = await page.waitForSelector('#btn-login')
+    const logInButton = await page.waitForSelector('#btn-login');
     await Promise.all([
       logInButton.click(),
       page.waitForNavigation()
-    ])
+    ]);
 
-    const cookies = (await client.send('Network.getAllCookies')).cookies
-    return cookies
+    const cookies = (await client.send('Network.getAllCookies')).cookies;
+    return cookies;
   }
 
-  const isLoggedIn = page.url().includes('/messaging')
+  const isLoggedIn = page.url().includes('/messaging');
 
   if (!isLoggedIn) {
-    throw new Error('Detected invalid or expired cookies')
+    throw new Error('Detected invalid or expired cookies');
   }
 
-  const cookies = (await client.send('Network.getAllCookies')).cookies
-  return cookies
+  const cookies = (await client.send('Network.getAllCookies')).cookies;
+  return cookies;
 }
 
 /**
  * Select a conversation using recipient info
  * @param {object} page      Puppeteer browser page
- * @param {string} recipient Recipient contact info
+ * @param {string} recipient Recipient info
  */
 module.exports.selectConversation = async(page, recipient) => {
   await Promise.all([
     page.goto('https://www.textnow.com/messaging'),
     page.waitForNavigation({ waitUntil: 'networkidle2' })
-  ])
+  ]);
 
-  await page.$eval('#newText', element => element.click())
+  await page.$eval('#newText', element => element.click());
 
-  const recipientField = await page.waitForSelector('.newConversationTextField')
-  await recipientField.type(recipient)
-  await page.keyboard.press('Enter')
+  const recipientField = await page.waitForSelector('.newConversationTextField');
+  await recipientField.type(recipient);
+  await page.keyboard.press('Enter');
 }
 
 /**
@@ -61,8 +61,8 @@ module.exports.selectConversation = async(page, recipient) => {
  * @param {number} delay   Optional delay after sending message (ms)
  */
 module.exports.sendMessage = async (page, message, delay) => {
-  const messageField = await page.waitForSelector('#text-input')
-  await messageField.type(message)
-  await page.keyboard.press('Enter')
-  await page.waitFor(delay || 500)
+  const messageField = await page.waitForSelector('#text-input');
+  await messageField.type(message);
+  await page.keyboard.press('Enter');
+  await page.waitFor(delay || 500);
 }
